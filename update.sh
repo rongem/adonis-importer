a9d8d4202cff8b7566a1a81ca09f4c41253cb3f6
Michael Ronge <devel@ronge.org>
2025-08-07 20:20:47 +0200

    ng update

---
+++ update.sh
@@ -0,0 +1,12 @@
+cd importer
+echo Updating @angular
+ng update @angular/cli @angular/core @ngrx/store --force --allow-dirty
+echo Updating other libraries
+rem npm install https://cdn.sheetjs.com/xlsx-latest/xlsx-latest.tgz
+npm update
+npm audit fix
+rem grunt bump
+npm prune
+npm outdated
+echo Done!
+read -n 1
